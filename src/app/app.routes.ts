58be407e88ea7887b8b60f4e89364aa9d66eef4c
Shuki <rabbishuki@gmail.com>
2025-06-20 02:57:38 +0300

    Feature: Add Pages to app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  // { path: 'data', component: DataPageComponent },
+  // { path: 'analysis', component: AnalysisPageComponent },
+  // { path: 'monitor', component: MonitorPageComponent },
+  { path: '**', redirectTo: '/' }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatTabLink, MatTabNav, MatTabNavPanel } from '@angular/material/tabs';
+import { MatToolbar } from '@angular/material/toolbar';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'biks-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatTabLink, MatToolbar, MatTabNav, MatTabNavPanel, NgOptimizedImage],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'trainee-results-dashboard';
 }
